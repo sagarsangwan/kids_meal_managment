e9919c02af55d18dd119d09da33e51b462e2fe7f
sagarsangwan <iamsagarsangwan@gmail.com>
2022-03-25 15:06:57 +0530

    added new pages

--- records_manager/urls.py
+++ records_manager/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
--- records_manager/views.py
+++ records_manager/views.py
@@ -5,3 +5,29 @@ from django.shortcuts import render
 def home(request):
 
     return render(request, 'pages/home.html')
+
+
+def signup(request):
+    print(request)
+    if request.method == 'POST':
+        print(request)
+        user_name = request.POST['user_name']
+        user_email = request.POST['user_email']
+        user_password = request.POST['user_password']
+        confirm_password = request.POST['confirm_password']
+        print(user_name, user_email, user_password, confirm_password)
+
+    return render(request, 'pages/signup.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # if user is not None:
+        #     login(request, user)
+        #     return redirect('home')
+        # else:
+        #     return redirect('login')
+    return render(request, 'pages/login.html')
