b8fb4710b04546b1ee57e672bab188a1c317125a
sagarsangwan <iamsagarsangwan@gmail.com>
2022-03-26 01:27:30 +0530

    new model added for kid meal

--- records_manager/models.py
+++ records_manager/models.py
@@ -15,3 +15,22 @@ class child(models.Model):
 
     class Meta:
         db_table = "child"
+
+
+class kid_meal(models.Model):
+    kid_id = models.ForeignKey('child', on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+    approved_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    food_group = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "kid_meal"
+
+    def __str__(self):
+        return self.meal_id
+
+    class Meta:
+        db_table = "kid_meal"
