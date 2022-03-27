24e7007ef6594bdd1ec8d1a47529e8c0981dc5f3
Sagar Sangwan <iamsagarsangwan@gmail.com>
2022-03-27 11:43:44 +0530

    Update settings.py

--- kids_meal_managment/settings.py
+++ kids_meal_managment/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
